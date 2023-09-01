f6a2659f33b69909b0259198ff2b065d5622c4bb
Jose daniel martinez <danieluz3188@gmail.com>
2023-08-31 19:44:19 -0500

    ""

--- src/common/buildAppTheme.ts
+++ src/common/buildAppTheme.ts
@@ -1,49 +1,56 @@
 // This is for adding types to the theme, so typescript can read them on use
-'@mui/material/styles' {
+'declare module '@mui/material/styles' {
+  interface PaletteColor {
+    generalBoxShadow: string;
+  }
+  interface TypeBackground {
+    transparent: string;
+  }
+}
 
-  export const buildAppTheme = () => ({
-    palette: {
-      primary: {
-        main: 'rgba(0, 26, 183, 1)',
-        generalBoxShadow: '0 1px 4px rgba(0, 0, 0, .16)',
-      },
-      background: {
-        default: 'rgb(255, 255, 255)',
-        transparent: 'rgba(255, 255, 255, 0)',
-        paper: 'rgba(241, 242, 243, 1)',
-      },
-      text: {
-        primary: 'rgb(13 13, 13)',
-        secondary: 'rgba(13, 13, 13, 0.7)',
-        primaryInverted: 'rgba(225, 225, 225, 1)',
-        secondaryInverted: 'rgba(225, 225, 225, 0.7)',
-      },    
+export const buildAppTheme = () => ({
+  palette: {
+    primary: {
+      main: 'rgba(0, 26, 183, 1)',
+      generalBoxShadow: '0 1px 4px rgba(0, 0, 0, .16)',
     },
-    typography: {
-      fontFamily: ['Open Sans', 'Roboto', 'Arial', 'sans-serif'].join(','),
-      h1: {
-        fontSize: '4rem',
-      },
-      h2: {
-        fontSize: '2rem',
-      },
-      h3: {
-        fontSize: '1.5rem',
-        fontWeight: '600',
-      },
-      h4: {
-        fontSize: '1.4rem',
-      },
-      h5: {
-        fontSize: '1.2rem',
-      },    
-      h6: {
-        fontSize: '1rem',
-      },     
-      caption: {
-        fontSize: '0.82rem',
-      },
+    background: {
+      default: 'rgb(255, 255, 255)',
+      transparent: 'rgba(255, 255, 255, 0)',
+      paper: 'rgba(241, 242, 243, 1)',
     },
+    text: {
+      primary: 'rgb(13 13, 13)',
+      secondary: 'rgba(13, 13, 13, 0.7)',
+      primaryInverted: 'rgba(225, 225, 225, 1)',
+      secondaryInverted: 'rgba(225, 225, 225, 0.7)',
+    },    
+  },
+  typography: {
+    fontFamily: ['Open Sans', 'Roboto', 'Arial', 'sans-serif'].join(','),
+    h1: {
+      fontSize: '4rem',
+    },
+    h2: {
+      fontSize: '2rem',
+    },
+    h3: {
+      fontSize: '1.5rem',
+      fontWeight: '600',
+    },
+    h4: {
+      fontSize: '1.4rem',
+    },
+    h5: {
+      fontSize: '1.2rem',
+    },    
+    h6: {
+      fontSize: '1rem',
+    },     
+    caption: {
+      fontSize: '0.82rem',
+    },
+  },
   // Here we can override MUI styles in their components
   // components: {
   //   MuiButton: {
