263aabca75d112fb0f31a1fc0db8321e30808b2b
Jose daniel martinez <danieluz3188@gmail.com>
2023-08-31 19:37:09 -0500

    ""

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -8,25 +8,19 @@ import Header from "../header";
 import Navigation from "../navigation";
 import styles from './Main.module.css';
 import { useFetchUser } from "../../hooks";
-import { BuyCredits } from "../buyCredits/BuyCredits";
-
+import { BuyCredits } from '../buyCredits/BuyCredits';
 export function Main() {
   const userIsLoading = useAppSelector(userSelectors.isLoading);
-
   useFetchUser();
-
   const [navigationIsMinimized, setNavigationIsMinimized] = useState(false);
-
   const toggleNavigationIsMinimized = () => {
     setNavigationIsMinimized((prev) => !prev);
   };
-
   if (userIsLoading) {
     return <LoadingDisplay type={loadingDisplayTypes.entireScreen} />;
   }
-
   return (
-    <div style={{ width: "100svw", height: "88svh" }}>
+    <div style={{ width: '100svw', height: '88svh' }}>
       <Header
         navigationIsMinimized={navigationIsMinimized}
         toggleNavigationIsMinimized={toggleNavigationIsMinimized}
@@ -34,27 +28,27 @@ export function Main() {
       <Navigation navigationIsMinimized={navigationIsMinimized} />
       <div
         className={`${styles.mainContentWrapper} ${
-          navigationIsMinimized ? styles.minimized : ""
+          navigationIsMinimized ? styles.minimized : ' '
         }`}
       >
         <Routes>
-          <Route path={"dashboard"} element={<></>} />
-          <Route path={"podcast-search"} element={<></>} />
-          <Route path={"live-events"} element={<></>} />
-          <Route path={"experts-search"} element={<></>} />
-          <Route path={"media-search"} element={<></>} />
-          <Route path={"conference-search"} element={<></>} />
-          <Route path={"my-lists"} element={<></>} />
-          <Route path={"outreach-sequences-mail"} element={<></>} />
-          <Route path={"account"} element={<></>} />
-          <Route path={"payment/credits"} element={<BuyCredits />} />
-          <Route path={"templates"} element={<></>} />
+          <Route path={'dashboard'} element={<></>} />
+          <Route path={'podcast-search'} element={<></>} />
+          <Route path={'live-events'} element={<></>} />
+          <Route path={'experts-search'} element={<></>} />
+          <Route path={'media-search'} element={<></>} />
+          <Route path={'conference-search'} element={<></>} />
+          <Route path={'my-lists'} element={<></>} />
+          <Route path={'outreach-sequences-mail'} element={<></>} />
+          <Route path={'account'} element={<></>} />
+          <Route path={'payment/credits'} element={<BuyCredits />} />
+          <Route path={'templates'} element={<></>} />
           <Route
-            path={"academy"}
+            path={'academy'}
             element={
               <div
-                id="candu-academy-content"
-                className="candu-academy-content"
+                id='candu-academy-content'
+                className='candu-academy-content'
               />
             }
           />
