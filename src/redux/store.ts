ca906547560196d1c39a88c0456c9f7180a373ce
Jose daniel martinez <danieluz3188@gmail.com>
2023-09-01 14:53:29 -0500

    ""

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-import { configureStore } from '@reduxjs/toolkit'
-import { cookiesSlice, cookiesStoreKey } from './cookies'
-import { alertsSlice, alertsStoreKey } from './alerts'
-import { authenticationSlice, authenticationStoreKey } from './authentication'
-import { userSlice, userStoreKey } from './user'
-import { contactListStoreKey } from './contactList/contactList.const'
-import { contactListSlice } from './contactList/contactList.slice'
-import { subscriptionSlice, subscriptionStoreKey } from './subscription'
- import { teamsSlice, teamsStoreKey } from './teams/index'
- import {
-   searchParametersSlice,
-   searchParametersStoreKey,
- } from './searchParameters'
-=======
 import { configureStore } from '@reduxjs/toolkit';
 import { cookiesSlice, cookiesStoreKey } from './cookies';
 import { alertsSlice, alertsStoreKey } from './alerts';
@@ -25,21 +10,19 @@ import {
   searchParametersSlice,
   searchParametersStoreKey,
 } from './searchParameters';
->>>>>>> origin/main
 
- export const store = configureStore({
-   reducer: {
-     [cookiesStoreKey]: cookiesSlice.reducer,
-     [alertsStoreKey]: alertsSlice.reducer,
-     [authenticationStoreKey]: authenticationSlice.reducer,
-     [userStoreKey]: userSlice.reducer,
-     [contactListStoreKey]: contactListSlice.reducer,
-     [subscriptionStoreKey]: subscriptionSlice.reducer,
-     [searchParametersStoreKey]: searchParametersSlice.reducer,
-     [teamsStoreKey]: teamsSlice.reducer,
-   },
- })
+export const store = configureStore({
+  reducer: {
+    [cookiesStoreKey]: cookiesSlice.reducer,
+    [alertsStoreKey]: alertsSlice.reducer,
+    [authenticationStoreKey]: authenticationSlice.reducer,
+    [userStoreKey]: userSlice.reducer,
+    [contactListStoreKey]: contactListSlice.reducer,
+    [subscriptionStoreKey]: subscriptionSlice.reducer,
+    [searchParametersStoreKey]: searchParametersSlice.reducer,
+  },
+});
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
