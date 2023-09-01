d845bd74e1c646161a11b3a822e034c8e7feb0b7
Jose daniel martinez <danieluz3188@gmail.com>
2023-09-01 14:33:47 -0500

    "Merge remote-tracking branch 'origin/main' into jose-Branch"

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { useAppSelector } from '../../redux/hooks'
@@ -10,34 +9,14 @@ import Navigation from '../navigation'
 import styles from './Main.module.css'
 import { useFetchUser } from '../../hooks'
 import { BuyCredits } from '../buyCredits/BuyCredits'
-=======
-import { useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { useAppSelector } from '../../redux/hooks';
-import { userSelectors } from '../../redux/user';
-import { LoadingDisplay } from '../../common';
-import { loadingDisplayTypes } from '../../types';
-import { useFetchUser } from '../../hooks';
-import Header from '../header';
-import Navigation from '../navigation';
-import BuyCredits from '../buyCredits';
-import Contacts from '../contacts';
-import styles from './Main.module.css';
-
->>>>>>> origin/main
 export function Main() {
   const userIsLoading = useAppSelector(userSelectors.isLoading)
   useFetchUser()
   const [navigationIsMinimized, setNavigationIsMinimized] = useState(false)
   const toggleNavigationIsMinimized = () => {
-<<<<<<< HEAD
-    setNavigationIsMinimized((prev) => !prev)
-  }
-=======
     setNavigationIsMinimized((prev) => !prev);
   };
 
->>>>>>> origin/main
   if (userIsLoading) {
     return <LoadingDisplay type={loadingDisplayTypes.entireScreen} />;
   }
@@ -50,29 +29,6 @@ export function Main() {
       <Navigation navigationIsMinimized={navigationIsMinimized} />
       <div
         className={`${styles.mainContentWrapper} ${
-<<<<<<< HEAD
-          navigationIsMinimized ? styles.minimized : ' '
-        }`}
-      >
-        <Routes>
-          <Route path={'dashboard'} element={<></>} />
-          <Route path={'podcast-search'} element={<></>} />
-          <Route path={'live-events'} element={<></>} />
-          <Route path={'experts-search'} element={<></>} />
-          <Route path={'media-search'} element={<></>} />
-          <Route path={'conference-search'} element={<></>} />
-          <Route path={'my-lists'} element={<></>} />
-          <Route path={'outreach-sequences-mail'} element={<></>} />
-          <Route path={'account'} element={<></>} />
-          <Route path={'payment/credits'} element={<BuyCredits />} />
-          <Route path={'templates'} element={<></>} />
-          <Route
-            path={'academy'}
-            element={
-              <div
-                id='candu-academy-content'
-                className='candu-academy-content'
-=======
           navigationIsMinimized ? styles.minimized : ''
         }`}
       >
@@ -95,16 +51,11 @@ export function Main() {
               <div
                 id="candu-academy-content"
                 className="candu-academy-content"
->>>>>>> origin/main
               />
             }
           />
         </Routes>
       </div>
     </div>
-<<<<<<< HEAD
   )
-=======
-  );
->>>>>>> origin/main
 }
